3c9b30e40282fe64c76a2d3cf730f0b8f9a20d57
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-09-07 19:58:06 -0700

    Add shadow to buttons

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -11,17 +11,17 @@ const Button = ({
 }) => {
   return (
     <motion.button
-      initial={false}
       whileHover={{
-        y: -5,
-        transition: { duration: 0.25, ease: "easeOut" },
+        y: -2,
+        transition: { duration: 0.2, ease: "easeOut" },
+        boxShadow: "",
       }}
       whileTap={{
         scale: 0.95,
-        rotate: 1.5,
-        transition: { duration: 0.25, ease: "easeOut" },
+        rotate: 2,
+        transition: { duration: 0.1, ease: "easeOut" },
       }}
-      className={`${primary ? "bg-primary text-background border-primary" : ""} ${accent ? "bg-accent text-background border-accent" : ""} ${tertiary ? "bg-surface border-text/30" : ""} relative cursor-pointer rounded-md border px-4 py-1 font-bold ${className}`}
+      className={`${primary ? "bg-primary text-background shadow-primary/40 border-primary" : ""} ${accent ? "bg-accent shadow-accent/40 text-background border-accent" : ""} ${tertiary ? "bg-surface shadow-surface/40 border-text/30" : ""} relative cursor-pointer rounded-md border px-4 py-1 font-bold hover:shadow-[0_4px_22px] ${className}`}
     >
       {children}
     </motion.button>
