f4158df09c02bc2a2c365bf529a5d8a769f0858d
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-09-15 15:15:52 -0700

    Add email

--- src/components/home/ContactLinks.jsx
+++ src/components/home/ContactLinks.jsx
@@ -3,35 +3,36 @@ import Link from "next/link";
 import Button from "../ui/Button";
 
 const links = [
-    { href: "https://github.com/amrxt1", name: "GitHub" },
-    { href: "https://linkedin.com/in/amritveer-singh", name: "LinkedIn" },
-    { href: "/resume", name: "Resume" },
-    { href: "/projects", name: "Projects" },
-    { href: "", name: "X" },
+  { href: "https://github.com/amrxt1", name: "GitHub" },
+  { href: "https://linkedin.com/in/amritveer-singh", name: "LinkedIn" },
+  { href: "/resume", name: "Resume" },
+  { href: "/projects", name: "Projects" },
+  { href: "", name: "X" },
+  { href: "mailto:amritv.dev@proton.me", name: "Email" },
 ];
 
 function ContactButton({ link }) {
-    return (
-        <Button tertiary>
-            <Link href={link.href}>{link.name}</Link>
-        </Button>
-    );
+  return (
+    <Button tertiary>
+      <Link href={link.href}>{link.name}</Link>
+    </Button>
+  );
 }
 
 const ContactLinks = () => {
-    return (
-        <Container>
-            <h3 className="text-primary mb-4 text-center text-2xl font-bold">
-                Contact
-            </h3>
-            <div className="flex flex-wrap justify-center gap-2">
-                <Button accent>Leave a Message</Button>
-                {links.map((link, i) => (
-                    <ContactButton link={link} key={i} />
-                ))}
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <h3 className="text-primary mb-4 text-center text-2xl font-bold">
+        Let's Work Together
+      </h3>
+      <div className="flex flex-wrap justify-center gap-2">
+        <Button accent>Leave a Message</Button>
+        {links.map((link, i) => (
+          <ContactButton link={link} key={i} />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default ContactLinks;
